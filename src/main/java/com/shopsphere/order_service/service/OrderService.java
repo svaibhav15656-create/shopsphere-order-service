1589ca6e11a2a5c9d493f0b8ea94ae6da42bf12c
S Vaibhav <s.vaibhav15656@gmail.com>
2026-07-15 00:17:32 +0530

    added kafka listener in orderservice

--- src/main/java/com/shopsphere/order_service/service/OrderService.java
+++ src/main/java/com/shopsphere/order_service/service/OrderService.java
@@ -3,13 +3,17 @@ package com.shopsphere.order_service.service;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.shopsphere.order_service.entity.Order;
 import com.shopsphere.order_service.entity.OrderStatus;
 import com.shopsphere.order_service.event.OrderEvent;
+import com.shopsphere.order_service.event.StockUpdateEvent;
 import com.shopsphere.order_service.repository.OrderRepository;
+
+import jakarta.transaction.Transactional;
 
 
 
@@ -31,5 +35,19 @@ public class OrderService {
         kafkaTemplate.send("order-events",event);
         return saveOrder;
     }
+    @Transactional
+    public void updateOrderStatus(Long orderId , boolean success){
+        Order order = orderRepository.findById(orderId)
+               .orElseThrow(() -> new RuntimeException("order not found" + orderId));
 
+        if(success){
+            order.setStatus(OrderStatus.CONFIRMED);
+        }else{
+            order.setStatus(OrderStatus.CANCELLED);
+        }
+    }
+    @KafkaListener(topics = "stock-update-events", groupId = "order-service-group")
+    public void handleStockUpdate(StockUpdateEvent event){
+        updateOrderStatus(event.getOrderId(), event.isSuccess());
+    }
 }
