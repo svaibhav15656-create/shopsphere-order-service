cf93580eb7d3337b003a950362aedc01ce7cca3a
S Vaibhav <s.vaibhav15656@gmail.com>
2026-07-20 23:17:49 +0530

    removed unnecessary imports

--- src/main/java/com/shopsphere/order_service/controller/OrderController.java
+++ src/main/java/com/shopsphere/order_service/controller/OrderController.java
@@ -6,7 +6,6 @@ import com.shopsphere.order_service.entity.Order;
 import com.shopsphere.order_service.security.JwtUtil;
 import com.shopsphere.order_service.service.OrderService;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
